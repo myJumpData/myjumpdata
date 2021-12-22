a2ec80eddde6043f032a7c2a512f8367b2999024
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2021-12-22 12:27:35 +0100

    reworked Button

--- apps/myjumpdata/src/components/Button.tsx
+++ apps/myjumpdata/src/components/Button.tsx
@@ -1,3 +1,5 @@
+import classNames from '../helper/classNames';
+
 type ButtonProps = {
   onClick?: any;
   name: string;
@@ -30,27 +32,38 @@ type ButtonIconProps = {
 export default function Button({ onClick, name, design, type }: ButtonProps) {
   let className = '';
   if (design === 'primary') {
-    className = 'bg-blue-500 text-white';
+    className =
+      'bg-blue-500 text-white dark:bg-transparent dark:outline-blue-500 dark:hover:bg-blue-700';
   } else if (design === 'secondary') {
-    className = 'bg-gray-500 text-white';
+    className =
+      'bg-gray-500 text-white dark:bg-transparent dark:outline-gray-500 dark:hover:bg-gray-700';
   } else if (design === 'success') {
-    className = 'bg-green-500 text-white';
+    className =
+      'bg-green-500 text-white dark:bg-transparent dark:outline-green-500 dark:hover:bg-green-700';
   } else if (design === 'danger') {
-    className = 'bg-red-500 text-white';
+    className =
+      'bg-red-500 text-white dark:bg-transparent dark:outline-red-500 dark:hover:bg-red-700';
   } else if (design === 'warning') {
-    className = 'bg-yellow-500 text-white';
+    className =
+      'bg-yellow-500 text-white dark:bg-transparent dark:outline-yellow-500 dark:hover:bg-yellow-700';
   } else if (design === 'light') {
-    className = 'bg-gray-200 text-black';
+    className =
+      'bg-gray-200 text-black dark:bg-transparent dark:outline-gray-200 dark:hover:bg-gray-400';
   } else if (design === 'dark') {
-    className = 'bg-gray-900 text-white';
+    className =
+      'bg-gray-900 text-white dark:bg-transparent dark:outline-gray-900 dark:hover:bg-gray-700';
   } else if (design === 'link') {
-    className = 'bg-gray-100 text-blue-500 text-sm';
+    className =
+      'bg-gray-100 text-blue-500 text-sm dark:bg-transparent dark:outline-gray-100 dark:hover:bg-gray-300';
   }
   return (
     <button
       onClick={onClick}
       type={type}
-      className={className + ' w-full rounded shadow h-10'}
+      className={classNames(
+        className,
+        'w-full rounded shadow dark:shadow-none h-8 outline-none dark:outline-1 my-2'
+      )}
     >
       {name}
     </button>
