05b41ad1d212944e58ff7f531fd6a4e1649076b6
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-01-11 21:50:59 +0100

    added middleware

--- apps/api/src/routes/users.routes.ts
+++ apps/api/src/routes/users.routes.ts
@@ -1,9 +1,45 @@
-import {Express} from "express-serve-static-core";
-import {getUsers, updateUser, updateUsersRole} from "../controllers/users.controller";
-import verifyToken from "../middlewares/authJwt";
+import { Express } from 'express-serve-static-core';
+import {
+  getUsers,
+  updateUser,
+  updateUsersRole,
+} from '../controllers/users.controller';
+import verifyToken from '../middlewares/authJwt';
+import {
+  bodySanitizeEmail,
+  bodySanitizeFirstname,
+  bodySanitizeLastname,
+  bodySanitizePassword,
+  bodySanitizeUsername,
+} from '../middlewares/bodySanitize';
+import {
+  bodyValidateEmail,
+  bodyValidateFirstname,
+  bodyValidateLastname,
+  bodyValidatePassword,
+  bodyValidateUsername,
+} from '../middlewares/bodyValidate';
 
 export default function UsersRoutes(app: Express) {
-  app.put("/users/role", [verifyToken], updateUsersRole);
-  app.get("/users", [verifyToken], getUsers);
-  app.put("/user", [verifyToken], updateUser);
+  app.put('/users/role', [verifyToken], updateUsersRole);
+  app.get('/users', [verifyToken], getUsers);
+  app.put(
+    '/user',
+    [
+      verifyToken,
+
+      bodySanitizeUsername,
+      bodySanitizeFirstname,
+      bodySanitizeLastname,
+      bodySanitizeEmail,
+      bodySanitizePassword,
+
+      bodyValidateUsername,
+      bodyValidateFirstname,
+      bodyValidateLastname,
+      bodyValidateEmail,
+      bodyValidatePassword,
+    ],
+    updateUser
+  );
 }
