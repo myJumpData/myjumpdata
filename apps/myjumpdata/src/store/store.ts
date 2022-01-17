a0150c5e824e2ee55cc1ff32bf48646edef880d7
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-01-17 23:34:21 +0100

    cleanup

--- apps/myjumpdata/src/store/store.ts
+++ apps/myjumpdata/src/store/store.ts
@@ -1,13 +1,12 @@
 import { AnyAction, createStore, Reducer } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { persistReducer, persistStore } from "redux-persist";
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./rootReducer";
 
 const store = createStore(
   persistReducer(
-    { key: "root", storage: storage, stateReconciler: autoMergeLevel2 },
+    { key: "root", storage: storage },
     rootReducer as Reducer<unknown, AnyAction>
   ),
   composeWithDevTools()
