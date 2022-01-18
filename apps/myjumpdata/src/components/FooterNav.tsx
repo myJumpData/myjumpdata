f36cf2ecff68e77c273d2ffb56750f4e86403f44
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-01-18 21:49:15 +0100

    fixed footer contrast ratio

--- apps/myjumpdata/src/components/FooterNav.tsx
+++ apps/myjumpdata/src/components/FooterNav.tsx
@@ -1,19 +1,19 @@
-import { useTranslation } from 'react-i18next';
-import { GiSkippingRope } from 'react-icons/gi';
-import { Link } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
+import { GiSkippingRope } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 export default function FooterNav({ social, links }) {
   const { t } = useTranslation();
 
   return (
-    <footer className="py-8 text-gray-400 dark:text-gray-600 px-4 sm:px-8 lg:px-12">
-      <div className="max-w-7xl mx-auto flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 py-4 border-b border-gray-400 dark:border-gray-600">
+    <footer className="py-8 text-gray-600 dark:text-gray-400 px-4 sm:px-8 lg:px-12">
+      <div className="max-w-7xl mx-auto flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 py-4 border-b border-gray-600 dark:border-gray-400">
         <div className="flex shrink-0 flex-col space-y-4">
           <div className="text-4xl">
             <GiSkippingRope />
           </div>
           <div className="text-sm">
-            <p>{t('common:footer.text')}</p>
+            <p>{t("common:footer.text")}</p>
           </div>
           <div className="flex space-x-2 text-lg">
             {social.map(({ link, icon }) => (
