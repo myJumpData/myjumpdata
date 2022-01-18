954a6d5adf8fd50822b118f5edd9813c3d509157
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-01-18 21:52:23 +0100

    remove redux dev tools from build

--- apps/myjumpdata/src/store/store.ts
+++ apps/myjumpdata/src/store/store.ts
@@ -4,12 +4,22 @@ import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./rootReducer";
 
-const store = createStore(
-  persistReducer(
-    { key: "root", storage: storage },
-    rootReducer as Reducer<unknown, AnyAction>
-  ),
-  composeWithDevTools()
-);
+let store: any;
+if (process.env.NODE_ENV === "development") {
+  store = createStore(
+    persistReducer(
+      { key: "root", storage: storage },
+      rootReducer as Reducer<unknown, AnyAction>
+    ),
+    composeWithDevTools()
+  );
+} else {
+  store = createStore(
+    persistReducer(
+      { key: "root", storage: storage },
+      rootReducer as Reducer<unknown, AnyAction>
+    )
+  );
+}
 export const persistor = persistStore(store);
 export default store;
