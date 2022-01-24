48b4bbdc9f86fb5055f999990ed560f8caefde27
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-01-24 10:56:37 +0100

    fixed jwt expiration

--- libs/const/src/lib/auth.ts
+++ libs/const/src/lib/auth.ts
@@ -3,5 +3,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const JWT_SECRET = process.env["JWT_SECRET"];
-export const JWT_EXPIRATION = 86400; // 31 Days
-export const JWT_REFRESH = 8035200; // 31 * 3 Days
+export const JWT_EXPIRATION = 30758400; // 356 Days
