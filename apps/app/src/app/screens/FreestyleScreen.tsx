64268b5a6b4b1e25a3eabc02b3c3c18fd1fa99b0
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-02-04 20:20:55 +0100

    added breadcrum to Freestyle

--- apps/app/src/app/screens/FreestyleScreen.tsx
+++ apps/app/src/app/screens/FreestyleScreen.tsx
@@ -59,6 +59,7 @@ export default function FreestyleScreen() {
   function getCurrentData() {
     getFreestyle(freestyle).then((response: any) => {
       setFolderData(response.data);
+      setRefreshing(false);
     });
   }
 
@@ -132,6 +133,7 @@ export default function FreestyleScreen() {
             alignItems: "center",
           }}
           onPress={() => {
+            setRefreshing(true);
             setFreestyle(item.key);
           }}
         >
@@ -168,6 +170,78 @@ export default function FreestyleScreen() {
 
   return (
     <StyledView>
+      <View
+        style={{
+          padding: 10,
+          flexDirection: "row",
+          alignContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            setFreestyle("");
+          }}
+          style={{
+            justifyContent: "center",
+          }}
+        >
+          <Ionicons
+            name={freestyle === "" ? "home" : "home"}
+            size={20}
+            color={Colors.grey}
+          />
+        </TouchableOpacity>
+        {freestyle !== "" &&
+          freestyle.split("_").map((e, index, array) => {
+            const last = index + 1 === array.length;
+            return (
+              <React.Fragment key={index}>
+                <View
+                  style={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Ionicons
+                    name="chevron-forward"
+                    size={20}
+                    color={Colors.grey}
+                  />
+                </View>
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => {
+                    setRefreshing(true);
+                    setFreestyle(
+                      freestyle
+                        .split("_")
+                        .splice(0, index + 1)
+                        .join("_")
+                    );
+                  }}
+                  style={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      color: Colors.grey,
+                      fontWeight: last ? "900" : "400",
+                      transform: [
+                        { scale: last ? 1.2 : 1 },
+                        { translateY: last ? -0.5 : 0 },
+                        { translateX: last ? 2 : 0 },
+                      ],
+                    }}
+                  >
+                    {t(`freestyle:${e}`)}
+                  </Text>
+                </TouchableOpacity>
+              </React.Fragment>
+            );
+          })}
+      </View>
       <FlatList
         renderItem={renderItem}
         data={folderData}
