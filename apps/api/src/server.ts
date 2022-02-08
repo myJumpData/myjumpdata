55e70a2b8dd5438023f2af0279f79e3e669272ae
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-02-08 22:08:03 +0100

    load freestyle from DB

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,8 @@ import { APP_URL } from "@myjumpdata/consts";
 import cors from "cors";
 import express from "express";
 import * as fs from "fs";
+import Translation from "./models/translation.model";
+import { requestHandlerError } from "./requestHandler";
 import FreestyleRoutes from "./routes/freestyle.routes";
 import GroupsRoutes from "./routes/groups.routes";
 import ScoredataRoutes from "./routes/scoredata.routes";
@@ -29,15 +31,52 @@ export default function createServer() {
   app.get("/locales/:lng/:ns", (req, res) => {
     const lng = req.params.lng;
     const ns = req.params.ns;
-    fs.readFile(
-      "./apps/api/src/locales/" + lng + "/" + ns + ".json",
-      (err, data) => {
+    Translation.find({ language: lng, namespace: ns })
+      .select("-_id -__v -language -namespace")
+      .exec((err, translation_data) => {
         if (err) {
-          return res.status(500).send(err);
+          return requestHandlerError(res, err);
         }
-        return res.status(200).send(data);
-      }
-    );
+        if (translation_data.length > 0) {
+          const map_data = {};
+          translation_data.forEach(
+            ({ key, translation }: { key: string; translation: string }) => {
+              return (map_data[key] = translation);
+            }
+          );
+          return res.status(200).send(map_data);
+        }
+        fs.readFile(
+          "./apps/api/src/locales/" + lng + "/" + ns + ".json",
+          "utf-8",
+          (err, data) => {
+            if (err) {
+              console.log(err);
+              return res.status(500).send(err);
+            }
+            const parse = JSON.parse(data);
+            /* if (ns === "freestyle") {
+          Object.keys(parse).map((key) => {
+            const t = parse[key];
+            console.log(key, t);
+            const tmp = new Translation({
+              language: lng,
+              namespace: ns,
+              key: key,
+              translation: t,
+            });
+            tmp.save((err, response) => {
+              if (err) {
+                return requestHandlerError(res, err);
+              }
+              console.log(response);
+            });
+          });
+        } */
+            return res.status(200).send(parse);
+          }
+        );
+      });
   });
   return app;
 }
