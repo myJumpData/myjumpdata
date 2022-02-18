f7a7c7481a379d771e6f4376f002986dab7210f8
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-02-18 11:48:49 +0100

    translated admin nav

--- apps/myjumpdata/src/components/AdminNav.tsx
+++ apps/myjumpdata/src/components/AdminNav.tsx
@@ -1,20 +1,31 @@
 import { classNames } from "@myjumpdata/utils";
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function AdminNav() {
   const route = useSelector((state: any) => state.route);
+  const { t } = useTranslation();
 
   const adminRoutes = [
-    { name: "home", current: route.match(new RegExp("admin/home(.*)")) },
-    { name: "users", current: route.match(new RegExp("admin/users(.*)")) },
-    { name: "groups", current: route.match(new RegExp("admin/groups(.*)")) },
     {
-      name: "freestyle",
+      name: t("common:nav_home"),
+      current: route.match(new RegExp("admin/home(.*)")),
+    },
+    {
+      name: t("common:nav_users"),
+      current: route.match(new RegExp("admin/users(.*)")),
+    },
+    {
+      name: t("common:nav_groups"),
+      current: route.match(new RegExp("admin/groups(.*)")),
+    },
+    {
+      name: t("common:nav_freestyle"),
       current: route.match(new RegExp("admin/freestyle(.*)")),
     },
     {
-      name: "localization",
+      name: t("common:nav_localization"),
       current: route.match(new RegExp("admin/localization(.*)")),
     },
   ];
@@ -23,7 +34,7 @@ export default function AdminNav() {
     <div className="min-w-[10rem] pl-2 text-black  dark:text-white">
       <div className="flex flex-row md:flex-col">
         <Link to="/admin" className="py-2 px-4 text-lg font-bold">
-          admin
+          {t("common:nav_admin")}
         </Link>
         <div className="flex flex-row overflow-y-auto md:flex-col">
           {adminRoutes.map((item: any) => (
