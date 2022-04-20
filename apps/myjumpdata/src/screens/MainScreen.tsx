bb9b14bb2de92032d883221fde273d207780fc8d
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-04-20 16:49:34 +0200

    fixed main page

--- apps/myjumpdata/src/screens/MainScreen.tsx
+++ apps/myjumpdata/src/screens/MainScreen.tsx
@@ -12,33 +12,33 @@ export default function MainScreen() {
 
   return (
     <div className="flex flex-col space-y-12">
-      <div className="mx-auto w-full max-w-screen-md flex-row sm:flex">
-        <div className="sm:w-1/2">
-          <h1 className="my-8 text-2xl font-bold leading-loose lg:text-4xl">
+      <div className="mx-auto w-full max-w-screen-md">
+        <div className="flex flex-col items-center sm:flex-row">
+          <h1 className="my-8 shrink-0 grow text-2xl font-bold leading-loose lg:text-4xl">
             <Trans i18nKey="main:header">
               <br />
               <br />
             </Trans>
           </h1>
-          <p className="my-8 text-lg">{t<string>("main:header_text")}</p>
-          <div className="my-8 flex">
-            <a href="https://play.google.com/store/apps/details?id=me.fediv.myjumpdata&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-              <img
-                className="h-16"
-                alt="Get it on Google Play"
-                src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-              />
-            </a>
-          </div>
-        </div>
-        <div className="hidden items-center justify-center p-4 sm:flex sm:w-1/2 sm:p-16">
           <img
-            className="w-full rounded-[25%] bg-gray-500/50"
+            className="max-h-[16rem] w-full max-w-[16rem] shrink rounded-[25%] bg-gray-500/50"
             src={Logo}
             alt="myJumpData"
             height="2rem"
             width="2rem"
           />
+        </div>
+        <p className="my-8 text-center text-lg sm:max-w-[30ch] sm:text-left">
+          {t<string>("main:header_text")}
+        </p>
+        <div className="my-8 flex justify-center sm:justify-start">
+          <a href="https://play.google.com/store/apps/details?id=me.fediv.myjumpdata&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+            <img
+              className="h-16"
+              alt="Get it on Google Play"
+              src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+            />
+          </a>
         </div>
       </div>
       <div className="mx-auto flex w-full max-w-screen-lg flex-col flex-wrap sm:flex-row">
