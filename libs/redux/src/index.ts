6c9f615d483a06827c69e2c6a0e4d641c24f8d8a
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-04-25 14:49:15 +0200

    commit

--- libs/redux/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./lib/freestyle.action";
-export * from "./lib/message.action";
-export * from "./lib/route.action";
-export * from "./lib/store";
-export * from "./lib/user.action";
--- libs/service/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./lib/auth.service";
-export * from "./lib/freestyle.service";
-export * from "./lib/groups.service";
-export * from "./lib/locales.service";
-export * from "./lib/scoredata.service";
-export * from "./lib/users.service";
