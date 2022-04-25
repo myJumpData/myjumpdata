2a07848fa9797b082423f5bff977fd4d779e9432
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-04-25 16:52:20 +0200

    added addional text variants

--- apps/app/src/app/components/StyledText.tsx
+++ apps/app/src/app/components/StyledText.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { Text } from "react-native";
+import { Text, TextProps } from "react-native";
 import useColorScheme from "react-native/Libraries/Utilities/useColorScheme";
 import { Colors, Font } from "../Constants";
 
-export function StyledText(props) {
+export function StyledText(props: TextProps) {
   const isDarkMode = useColorScheme() === "dark";
   return (
     <Text
@@ -18,7 +18,38 @@ export function StyledText(props) {
     />
   );
 }
-export function StyledTextH1(props) {
+export function StyledShyText(props: TextProps) {
+  return (
+    <Text
+      {...props}
+      style={[
+        {
+          color: Colors.grey,
+          fontSize: Font.size,
+        },
+        props.style,
+      ]}
+    />
+  );
+}
+export function StyledHeading(props: TextProps) {
+  const isDarkMode = useColorScheme() === "dark";
+  return (
+    <Text
+      {...props}
+      style={[
+        {
+          marginTop: 10,
+          color: isDarkMode ? Colors.white : Colors.black,
+          fontSize: 20,
+          fontWeight: "700",
+        },
+        props.style,
+      ]}
+    />
+  );
+}
+export function StyledTextH1(props: TextProps) {
   const isDarkMode = useColorScheme() === "dark";
   return (
     <Text
