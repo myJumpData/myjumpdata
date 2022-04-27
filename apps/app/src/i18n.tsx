b469502951a0871f65756819ad62f15e2c5dc659
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-04-27 18:43:04 +0200

    i18n multiload

--- apps/app/src/i18n.tsx
+++ apps/app/src/i18n.tsx
@@ -1,12 +1,13 @@
 import i18n from "i18next";
-import i18nHttpLoader from "i18next-http-backend";
+import Http from "i18next-http-backend";
+import BackendAdapter from "i18next-multiload-backend-adapter";
 import { initReactI18next } from "react-i18next";
 import { getLocales } from "react-native-localize";
 import { LANGUAGES, NAMESPACES } from "./app/Constants";
 import getApi from "./app/utils/getApi";
 
 i18n
-  .use(i18nHttpLoader)
+  .use(BackendAdapter)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
@@ -21,6 +22,9 @@ i18n
     lng: getLocales()[0].languageCode,
     compatibilityJSON: "v3",
     backend: {
-      loadPath: `${getApi()}/locales/{{lng}}/{{ns}}`,
+      backend: Http,
+      backendOption: {
+        loadPath: `${getApi()}/locales/{{lng}}/{{ns}}`,
+      },
     },
-  });
+  } as any);
--- apps/myjumpdata/src/i18n.tsx
+++ apps/myjumpdata/src/i18n.tsx
@@ -1,13 +1,14 @@
 import i18n from "i18next";
 import detector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
+import Http from "i18next-http-backend";
+import BackendAdapter from "i18next-multiload-backend-adapter";
 import { initReactI18next } from "react-i18next";
 import { LANGUAGES, NAMESPACES } from "./Constants";
 import getApi from "./utils/getApi";
 
 i18n
   .use(detector)
-  .use(HttpApi)
+  .use(BackendAdapter)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
@@ -20,10 +21,13 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: `${getApi()}/locales/{{lng}}/{{ns}}`,
+      backend: Http,
+      backendOption: {
+        loadPath: `${getApi()}/locales/{{lng}}/{{ns}}`,
+      },
     },
     detection: {
       order: ["localStorage", "cookie", "navigator"],
       caches: ["localStorage", "cookie"],
     },
-  });
+  } as any);
