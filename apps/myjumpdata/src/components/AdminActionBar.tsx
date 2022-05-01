c6a349541a6c15569a37a50012574a3214e804b0
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-05-01 15:49:11 +0200

    fixed AdminActionBar Unique key

--- apps/myjumpdata/src/components/AdminActionBar.tsx
+++ apps/myjumpdata/src/components/AdminActionBar.tsx
@@ -9,8 +9,9 @@ export default function AdminActionBar({
     <div className="flex h-12 items-center justify-between space-x-4 rounded-lg bg-gray-500/50 px-4 py-2">
       <span className="text-xl font-bold">{text}</span>
       <div className="flex space-x-4">
-        {actions?.map((action) => (
+        {actions?.map((action, index) => (
           <div
+            key={index}
             className="opacity-50 transition hover:scale-125 hover:opacity-100"
             onClick={() => {
               action.onClick();
