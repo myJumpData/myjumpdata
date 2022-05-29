642da6539f9d30d7b677cd92062bd426f5aa9bc5
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-05-29 21:57:34 +0200

    table view Freestyle

--- apps/myjumpdata/src/screens/FreestyleGroupScreen.tsx
+++ apps/myjumpdata/src/screens/FreestyleGroupScreen.tsx
@@ -8,10 +8,17 @@ import { Back, Element, Folder } from "../components/Freestyle";
 import { SelectInput } from "../components/Input";
 import { setFreestyle } from "../redux/freestyle.action";
 import { setRoute } from "../redux/route.action";
-import { getFreestyle, getFreestyleData } from "../service/freestyle.service";
+import {
+  getFreestyle,
+  getFreestyleData,
+  saveFreestyleData,
+} from "../service/freestyle.service";
 import { getClub, getGroup } from "../service/groups.service";
 import { capitalize } from "../utils/capitalize";
 import fullname from "../utils/fullname";
+import useBreakpoint from "../hooks/useBreakpoint";
+import Spinner from "../components/Spinner";
+import { FaRegCheckSquare, FaRegSquare, FaSquare } from "react-icons/fa";
 
 type freestyle_folder_data = {
   id: string;
@@ -39,9 +46,11 @@ export default function FreestyleGroupScreen() {
   const freestyle = useSelector((state: any) => state.freestyle);
 
   const [freestyleDataUser, setFreestyleDataUser] = useState<any[]>([]);
+  const [freestyleDataUsers, setFreestyleDataUsers] = useState<any>({});
   const [folderData, setFolderData] = useState<freestyle_folder_data[]>([]);
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const breakpoint = useBreakpoint();
 
   useEffect(() => {
     getGroup(params.id as string).then((response: any) => {
@@ -61,12 +70,18 @@ export default function FreestyleGroupScreen() {
   useEffect(() => {
     getUserData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userSelected]);
+  }, [userSelected, breakpoint, userSelect]);
+
+  function getCurrentData() {
+    getFreestyle(freestyle).then((response: any) => {
+      setFolderData(response.data);
+    });
+  }
 
   useEffect(() => {
     getCurrentData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [freestyle]);
+  }, [freestyle, breakpoint]);
 
   function getUserData() {
     getClub().then((response) => {
@@ -74,19 +89,25 @@ export default function FreestyleGroupScreen() {
         navigate(-1);
       }
     });
-    if (userSelected) {
-      return getFreestyleData(userSelected).then((response: any) => {
-        setFreestyleDataUser(response.data);
-        return Promise.resolve();
+    if (breakpoint === "xs" || breakpoint === "sm" || breakpoint === "md") {
+      if (userSelected) {
+        return getFreestyleData(userSelected).then((response: any) => {
+          setFreestyleDataUser(response.data);
+          return Promise.resolve();
+        });
+      }
+      return Promise.resolve();
+    } else {
+      const list = userSelect?.map((value: any) => {
+        return getFreestyleData(value.value).then((response: any) => {
+          return response.data;
+        });
       });
+      Promise.all(list).then((res) => {
+        setFreestyleDataUsers(res.flat());
+      });
+      return Promise.resolve();
     }
-    return Promise.resolve();
-  }
-
-  function getCurrentData() {
-    getFreestyle(freestyle).then((response: any) => {
-      setFolderData(response.data);
-    });
   }
 
   return (
@@ -96,53 +117,181 @@ export default function FreestyleGroupScreen() {
           {t("freestyle_title") + " " + groupName}
         </span>
       </div>
-      <div className="mb-2 flex items-center space-x-2">
-        <div className="w-full">
-          <SelectInput
-            options={userSelect}
-            current={userSelected}
-            stateChange={setUserSelected}
-          />
+      {breakpoint === "xs" || breakpoint === "sm" || breakpoint === "md" ? (
+        <div className="mb-2 flex items-center space-x-2">
+          <div className="w-full">
+            <SelectInput
+              options={userSelect}
+              current={userSelected}
+              stateChange={setUserSelected}
+            />
+          </div>
         </div>
-      </div>
+      ) : null}
       <Breadcrumb
         data={freestyle ? freestyle.split("_") : []}
         setState={setFreestyle}
       />
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {folderData?.map((e: freestyle_folder_data) => {
-          if (e.element) {
-            return (
-              <Element
-                user={userSelected}
-                name={e.key}
-                level={e.level}
-                key={e.key}
-                id={e.id}
-                compiled={e.compiled}
-                element={
-                  freestyleDataUser?.find((i) => i.element === e.id) || {}
-                }
-                onRefresh={() => {
-                  return getUserData();
-                }}
-              />
-            );
-          } else if (e.back) {
-            return <Back to={e.key} key="back" state={setFreestyle} />;
-          } else {
-            return (
-              <Folder
-                key={e.key}
-                set={e.set}
-                name={e.key}
-                onClick={(el) => setFreestyle(el)}
-              />
-            );
-          }
-        })}
-      </div>
+      {breakpoint === "xs" || breakpoint === "sm" || breakpoint === "md" ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {folderData?.map((e: freestyle_folder_data) => {
+            if (e.element) {
+              return (
+                <Element
+                  user={userSelected}
+                  name={e.key}
+                  level={e.level}
+                  key={e.key}
+                  id={e.id}
+                  compiled={e.compiled}
+                  element={
+                    freestyleDataUser?.find((i) => i.element === e.id) || {}
+                  }
+                  onRefresh={() => {
+                    return getUserData();
+                  }}
+                />
+              );
+            } else if (e.back) {
+              return <Back to={e.key} key="back" state={setFreestyle} />;
+            } else {
+              return (
+                <Folder
+                  key={e.key}
+                  set={e.set}
+                  name={e.key}
+                  onClick={(el) => setFreestyle(el)}
+                />
+              );
+            }
+          })}
+        </div>
+      ) : (
+        <>
+          <Back
+            to={folderData?.find((i) => i.back)?.key || ""}
+            key="back"
+            state={setFreestyle}
+          />
+          <table>
+            <tr className="border-b border-gray-500/50">
+              <th></th>
+              {folderData?.filter((i) => i.element).length > 0
+                ? userSelect?.map(({ name }: { name: string }) => {
+                    return (
+                      <th className="rotate-180 p-2 [writing-mode:vertical-lr]">
+                        {name}
+                      </th>
+                    );
+                  })
+                : null}
+            </tr>
+            {folderData?.map((e: freestyle_folder_data) => {
+              if (e.element) {
+                return (
+                  <tr className="border-b border-gray-500/50">
+                    <td className="flex cursor-pointer flex-col py-2">
+                      <span className="text-lg font-bold leading-none">
+                        {e.compiled
+                          ? e.key
+                              .split("_")
+                              .map((item) => t(`freestyle:${item}`))
+                              .join(" ")
+                          : t(`freestyle:${e.key}`)}
+                      </span>
+                      {e.level ? (
+                        <span className="text-sm leading-none opacity-75">
+                          Lvl. {e.level}
+                        </span>
+                      ) : null}
+                    </td>
+                    {folderData?.filter((i) => i.element).length > 0
+                      ? userSelect?.map(({ value }: any) => {
+                          if (!freestyleDataUsers) {
+                            return null;
+                          }
+                          const el = freestyleDataUsers?.find(
+                            (i: any) => i.user === value && i.element === e.id
+                          );
+                          return (
+                            <th>
+                              <Check
+                                user={value}
+                                elementId={e.id}
+                                stateCoach={
+                                  !userSelect
+                                    ? undefined
+                                    : el?.stateCoach || false
+                                }
+                                stateUser={
+                                  !userSelect
+                                    ? undefined
+                                    : el?.stateUser || false
+                                }
+                                onRefresh={getUserData}
+                              />
+                            </th>
+                          );
+                        })
+                      : null}
+                  </tr>
+                );
+              } else if (e.group) {
+                return (
+                  <tr
+                    className="cursor-pointer border-b border-gray-500/50"
+                    onClick={() => {
+                      setFreestyle(e.key);
+                    }}
+                  >
+                    <td className="flex cursor-pointer flex-col py-2">
+                      <span className="text-lg font-bold leading-none">
+                        {t(`freestyle:${e.key.split("_").at(-1)}`)}
+                      </span>
+                      {e.set ? (
+                        <span className="text-sm leading-none opacity-75">
+                          {e.set ? "set" : null}
+                        </span>
+                      ) : null}
+                    </td>
+                  </tr>
+                );
+              }
+              return null;
+            })}
+          </table>
+        </>
+      )}
     </>
+  );
+}
+
+function Check({ user, elementId, stateCoach, stateUser, onRefresh }) {
+  const [reloading, setReloading] = useState(false);
+
+  return (
+    <div
+      onClick={() => {
+        setReloading(true);
+        saveFreestyleData(user, elementId as string, !stateCoach).then(() => {
+          onRefresh().then(() => {
+            setReloading(false);
+          });
+        });
+      }}
+    >
+      <span className="ml-2 flex items-center justify-center self-end py-2 xs:self-center xs:text-2xl">
+        {reloading || (stateUser === undefined && stateCoach === undefined) ? (
+          <Spinner />
+        ) : stateCoach ? (
+          <FaSquare />
+        ) : stateUser ? (
+          <FaRegCheckSquare />
+        ) : (
+          <FaRegSquare />
+        )}
+      </span>
+    </div>
   );
 }
