3c8545c335ade5b619fd77d7c948594873e9c27e
Marko Fediv <69723303+maggggoooooo@users.noreply.github.com>
2022-05-29 17:01:33 +0200

    fix

--- apps/app/src/app/redux/player.action.ts
+++ apps/app/src/app/redux/player.action.ts
@@ -4,37 +4,37 @@ import store from "./store";
 export async function playOrPause() {
   const state = await TrackPlayer.getState();
   if (state === State.Paused) {
-    TrackPlayer.play();
+    await TrackPlayer.play();
     return;
   }
   if (state === State.Playing) {
-    TrackPlayer.pause();
+    await TrackPlayer.pause();
     return;
   }
-  TrackPlayer.play();
+  await TrackPlayer.play();
 }
 export async function onPressNext() {
   const q = await TrackPlayer.getQueue();
   if (q.length > 1) {
-    TrackPlayer.skipToNext();
+    await TrackPlayer.skipToNext();
   } else {
-    TrackPlayer.seekTo(0);
+    await TrackPlayer.seekTo(0);
   }
 }
 export async function onPressPrev() {
   const position = await TrackPlayer.getPosition();
   if (position > 1) {
-    TrackPlayer.seekTo(0);
+    await TrackPlayer.seekTo(0);
     return;
   }
   const q = await TrackPlayer.getQueue();
   if (q.length > 1) {
-    TrackPlayer.skipToPrevious();
+    await TrackPlayer.skipToPrevious();
   }
 }
 
 export async function onPressStop() {
-  TrackPlayer.reset();
+  await TrackPlayer.stop();
 }
 
 export async function onPressRepeatMode() {
@@ -50,18 +50,18 @@ export async function onPressRepeatMode() {
 
 export async function setRepeatMode(mode: "off" | "track" | "queue") {
   if (mode === "off") {
-    TrackPlayer.setRepeatMode(RepeatMode.Off);
+    await TrackPlayer.setRepeatMode(RepeatMode.Off);
   }
   if (mode === "track") {
-    TrackPlayer.setRepeatMode(RepeatMode.Track);
+    await TrackPlayer.setRepeatMode(RepeatMode.Track);
   }
   if (mode === "queue") {
     const q = await TrackPlayer.getQueue();
     if (q.length > 1) {
-      TrackPlayer.setRepeatMode(RepeatMode.Queue);
+      await TrackPlayer.setRepeatMode(RepeatMode.Queue);
     } else {
       mode = "off";
-      TrackPlayer.setRepeatMode(RepeatMode.Off);
+      await TrackPlayer.setRepeatMode(RepeatMode.Off);
     }
   }
   return store.dispatch({ type: "setRepeatMode", payload: mode });
